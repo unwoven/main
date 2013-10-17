26ac2980d181608a9af0cae274274f37a4ac3191
Erik <unwoven@gmail.com>
2013-10-17 03:47:13 -0400

    more

--- main.py
+++ main.py
@@ -3,5 +3,10 @@
 import wxversion
 wxversion.select(['2.8.12'])
 
-import wx
+#import wx
 
+import subprocess
+
+print "\n", subprocess.Popen('ps -c -o pid,ppid,user', shell=True)
+
+print "\033[31;44mTEXT\033[39;49m"
